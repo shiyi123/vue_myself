edaf72a0f768755463aabde8a3783834b768f8cc
lily <lily@tsingoal.com>
2018-10-17 09:36:24 +0800

    first c

---
+++ src/components/First.vue
@@ -0,0 +1,32 @@
+<template>
+ <div id="bgo">
+ </div>
+</template>
+
+<script>
+ export default {
+   name:"First",
+   data () {
+     return {
+
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style>
+#bgo{
+     width: 100%;
+    height: 100%;
+    position: fixed;
+    background: url(/static/img/bg.40c72c6.jpg) no-repeat center;
+    background-size: 100% 100%;
+    top: 0;
+    margin: 0;
+    padding: 0;
+}
+ 
+</style>
